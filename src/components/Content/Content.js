23e297881e58a56cf1ca748856eb33827fc37aa6
Jake Dwyer <jakedwyer7@gmail.com>
2024-10-23 15:57:48 -0400

    added nugig link

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,7 +2,7 @@ import './Content.css';
 import '../../styles/Global.css';
 import '../../styles/Fonts.css';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function Content() {
     return (
@@ -12,7 +12,17 @@ function Content() {
                     CURRENTLY
                 </h1>
                 <p className='primaryText'>
-                I'm a third-year student at Northeastern University, studying Computer Science and Business Administration. Currently - I'm on Co-op with Northeastern, working on nugig.
+                    I'm a third-year student at Northeastern University, studying Computer Science and Business Administration. Currently, I'm on Co-op with Northeastern, working on <a href="https://nugig.tech/" target="_blank" rel="noopener noreferrer">nugig
+                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="Arrows">
+                                <g id="arrow.up.forward">
+                                    <g id="Group">
+                                        <path id="Vector" d="M12 9.43819L11.9958 2.77406C11.9958 2.31687 11.6953 2 11.2209 2H4.5592C4.11726 2 3.80428 2.33678 3.80428 2.72587C3.80428 3.11376 4.14445 3.43249 4.53863 3.43249H6.90095L9.75976 3.33465L8.55213 4.39864L2.2347 10.7316C2.08544 10.8833 2 11.0694 2 11.2549C2 11.6392 2.34973 12 2.74471 12C2.9355 12 3.11607 11.9254 3.26772 11.7694L9.59718 5.4443L10.6715 4.23057L10.5628 6.97664V9.4588C10.5628 9.85437 10.8802 10.199 11.2733 10.199C11.6634 10.199 12 9.86712 12 9.43819Z" fill='var(--primaryText)' />
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                    </a>.
                 </p>
             </div>
             <div className='contentBlock'>
@@ -20,11 +30,11 @@ function Content() {
                     ABOUT ME
                 </h1>
                 <p className='primaryText'>
-                I enjoy creating software that users can be passionate about using.
+                    I enjoy creating software that users can be passionate about using.
                 </p>
             </div>
             <SpeedInsights />
-            <Analytics/>
+            <Analytics />
         </div>
     );
 }
