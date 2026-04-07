4dfa75a06b5c596f5bf6d102452b6b575e9914be
jake-dwyer <dwyer.jak@northeastern.edu>
2026-04-07 03:02:53 -0400

    update curate

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,9 +18,12 @@ function Cards() {
                 <img src={Curate} alt="Curate"/>
                     <div className='cardContent'>
                         <h2>
-                            Curate [In Development]
+                            Curate	
+                            <a href="https://github.com/jake-dwyer/curate" target="_blank" rel="noreferrer">
+                            [In Development]
+                            </a>
                         </h2>
-                        <a href="https://github.com/jake-dwyer/curate" target="_blank" rel="noreferrer">
+                        <a href="https://curatemusic.app" target="_blank" rel="noreferrer">
                             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g id="Arrows">
                             <g id="arrow.up.forward">
