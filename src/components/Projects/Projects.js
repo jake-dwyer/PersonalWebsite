0b875faa548c02e2eb739bb2813bda7e3cb1c156
jake-dwyer <dwyer.jak@northeastern.edu>
2026-04-07 17:25:18 -0400

    again...

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,9 +18,8 @@ function Cards() {
                 <img src={Curate} alt="Curate"/>
                     <div className='cardContent'>
                         <h2>
-                            Curate
                             <a href="https://github.com/jake-dwyer/curate" target="_blank" rel="noreferrer">
-                             [In Development]
+                            Curate [In Development]
                             </a>
                         </h2>
                         <a href="https://curatemusic.app" target="_blank" rel="noreferrer">
